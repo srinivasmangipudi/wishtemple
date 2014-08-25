97d067026a506454671bb6310e8f02fed02733ae
Srinivas Mangipudi <shrinimann@gmail.com>
2014-08-25 18:17:19 +0530

    show me where I am button on map.

--- client/client.js
+++ client/client.js
@@ -482,6 +482,7 @@ function initMap()
     }]
   });
 
+  //geocode-locate control
   var geoCoderOptions = {
     collapsed: true, /* Whether its collapsed or not */
     position: 'topright', /* The position of the control */
@@ -498,6 +499,37 @@ function initMap()
   };
   var osmGeocoder = new L.Control.OSMGeocoder(geoCoderOptions);
   mapa.addControl(osmGeocoder);
+
+  //locate current location
+  L.control.locate({
+      position: 'topleft',  // set the location of the control
+      drawCircle: true,  // controls whether a circle is drawn that shows the uncertainty about the location
+      follow: false,  // follow the user's location
+      setView: true, // automatically sets the map view to the user's location, enabled if `follow` is true
+      keepCurrentZoomLevel: false, // keep the current map zoom level when displaying the user's location. (if `false`, use maxZoom)
+      stopFollowingOnDrag: false, // stop following when the map is dragged if `follow` is true (deprecated, see below)
+      remainActive: false, // if true locate control remains active on click even if the user's location is in view.
+      markerClass: L.circleMarker, // L.circleMarker or L.marker
+      circleStyle: {},  // change the style of the circle around the user's location
+      markerStyle: {},
+      followCircleStyle: {},  // set difference for the style of the circle around the user's location while following
+      followMarkerStyle: {},
+      icon: 'icon-location',  // `icon-location` or `icon-direction`
+      iconLoading: 'icon-spinner  animate-spin',  // class for loading icon
+      circlePadding: [0, 0], // padding around accuracy circle, value is passed to setBounds
+      metric: true,  // use metric or imperial units
+      onLocationError: function(err) {alert(err.message)},  // define an error callback function
+      onLocationOutsideMapBounds:  function(context) { // called when outside map boundaries
+              alert(context.options.strings.outsideMapBoundsMsg);
+      },
+      showPopup: true, // display a popup when the user click on the inner marker
+      strings: {
+          title: "Show me where I am",  // title of the locate control
+          popup: "You are within {distance} {unit} from this point",  // text to appear if user clicks on circle
+          outsideMapBoundsMsg: "You seem located outside the boundaries of the map" // default message for onLocationOutsideMapBounds
+      },
+      locateOptions: {}  // define location options e.g enableHighAccuracy: true or maxZoom: 10
+  }).addTo(mapa);
 }
 
 function addWishMarkersOnMap()
